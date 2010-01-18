926f014e14986f32a6342895326c04366314d77a
Jaroslav Kysela <perex@perex.cz>
2010-01-18 14:51:04 +0100

    alsatool: added 'kmerge' command, more info to comments.py
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- config.py
+++ config.py
@@ -8,8 +8,17 @@ USER = os.getenv('USER')
 VERBOSE = False
 GERRORS = 0
 TMPDIR = '/dev/shm/alsatool'
-GIT_KERNEL_MERGE = 'v2.6.30'
-GIT_DRIVER_MERGE = 'v1.0.19'
+SMTP_SERVER = 'localhost'
+GIT_KERNEL_MERGE = 'v2.6.32'
+GIT_DRIVER_MERGE = 'v1.0.21'
+GIT_MERGE_REPOS = [
+        ('git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git', 'master', 'linux-2.6', 'http://www.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git'),
+        ('git@git.alsa-project.org:alsa-kernel.git', 'fixes', 'perex-fixes', 'http://git.alsa-project.org/http/alsa-kernel.git'),
+        ('git@git.alsa-project.org:alsa-kernel.git', 'devel', 'perex-devel', 'http://git.alsa-project.org/http/alsa-kernel.git'),
+        ('git://git.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git', 'topic/misc', 'tiwai-topic-misc', 'http://www.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git'),
+        ('git://git.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git', 'topic/hda', 'tiwai-topic-hda', 'http://www.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git'),
+        ('git://git.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git', 'topic/asoc', 'tiwai-topic-asoc', 'http://www.kernel.org/pub/scm/linux/kernel/git/tiwai/sound-2.6.git')
+]
 REPOSITORIES = [
         'alsa', 'alsa-driver', 'alsa-kmirror', 'alsa-lib', 'alsa-utils',
         'alsa-tools', 'alsa-firmware', 'alsa-oss', 'alsa-plugins',
